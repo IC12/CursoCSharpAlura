d201e208d06a826f6b6e1c7f2383b5d28ccb684a
Iara Camargos <84880369+IC12@users.noreply.github.com>
2022-06-03 23:11:59 -0300

    Pilhas atts

---
+++ Collections/Collections1/Stack/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Coleção pilha
+            var navegador = new Navegador();
+
+            navegador.NavegarPara("google.com");
+            navegador.NavegarPara("caelum.com.br");
+            navegador.NavegarPara("alura.com.br");
+
+            navegador.Anterior();
+            navegador.Anterior();
+            navegador.Anterior();
+            navegador.Anterior();
+
+            navegador.Proximo();
+            navegador.Proximo();
+            navegador.Proximo();
+            navegador.Proximo();
+        }
+    }
+
+    internal class Navegador
+    {
+        private readonly Stack<string> historicoAnterior = new Stack<string>();
+
+        private readonly Stack<string> historicoProximo = new Stack<string>();
+
+        private string atual = "vazia";
+
+        public Navegador()
+        {
+            Console.WriteLine("Página atual: " + atual);
+        }
+
+        internal void Anterior()
+        {
+            //Any verifica se tem algum elemento na pilha
+            if (historicoAnterior.Any())
+            {
+                historicoProximo.Push(atual);
+                //obter valor de um elemento na pilha
+                atual = historicoAnterior.Pop();
+                Console.WriteLine("Página atual: " + atual);
+            }
+        }
+
+        internal void NavegarPara(string url)
+        {
+            //Colocar em uma pilha push
+            historicoAnterior.Push(atual);
+            atual = url;
+            Console.WriteLine("Página atual: " + atual);
+        }
+
+        internal void Proximo()
+        {
+            if (historicoProximo.Any())
+            {
+                historicoAnterior.Push(atual);
+                atual = historicoProximo.Pop();
+                Console.WriteLine("Página atual: " + atual);
+            }
+        }
+    }
+}
