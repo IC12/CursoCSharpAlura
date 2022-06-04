42b8d38589439c260a8b82fc868eae7a25680d90
Iara Camargos <84880369+IC12@users.noreply.github.com>
2022-06-03 23:08:01 -0300

    List atts

---
+++ Collections/Collections1/ListObjets/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var aulaIntro = new Aula("Introdução às Coleções", 20);
+            var aulaModelando = new Aula("Modelando a Classe Aula", 18);
+            var aulaSets = new Aula("Trabalhando com Conjuntos", 16);
+
+            List<Aula> aulas = new List<Aula>();
+            aulas.Add(aulaIntro);
+            aulas.Add(aulaModelando);
+            aulas.Add(aulaSets);
+
+            Imprimir(aulas);
+
+            aulas.Sort();
+            Imprimir(aulas);
+
+            //Ordenação por tempo de duração
+            aulas.Sort((este, outro) => este.Tempo.CompareTo(outro.Tempo));
+            Imprimir(aulas);
+        }
+
+        private static void Imprimir(List<Aula> aulas)
+        {
+            Console.Clear();
+            foreach (var aula in aulas)
+            {
+                Console.WriteLine(aula);
+            }
+        }
+    }
+
+    class Aula : IComparable
+    {
+        private string titulo;
+        private int tempo;
+
+        public Aula(string titulo, int tempo)
+        {
+            this.titulo = titulo;
+            this.tempo = tempo;
+        }
+
+        public string Titulo { get => titulo; set => titulo = value; }
+        public int Tempo { get => tempo; set => tempo = value; }
+
+        public int CompareTo(object obj)
+        {
+            Aula that = obj as Aula;
+            return this.titulo.CompareTo(that.titulo);
+        }
+
+        public override string ToString()
+        {
+            return $"Título: {titulo}, tempo: {tempo} minutos";
+        }
+    }
+}
