733381b2c585f708f4d9094c20ffa38b870b5af7
Iara Camargos <84880369+IC12@users.noreply.github.com>
2022-06-13 10:16:22 -0300

    Atts Collections2

---
+++ Collections/Collections2/JaggedArrays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace JaggedArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Jagged Array = Denteado, Serrilhado ou Array de arrays
+            string[][] familias = new string[3][];
+            //{
+            //    { "Fred", "Wilma", "Pedrita" },
+            //    { "Homer", "Marge", "Lisa", "Bart", "Maggie" },
+            //    { "Florinda", "Kiko" }
+            //};
+
+            familias[0] = new string[] { "Fred", "Wilma", "Pedrita" };
+            familias[1] = new string[] { "Homer", "Marge", "Lisa", "Bart", "Maggie" };
+            familias[2] = new string[] { "Florinda", "Kiko" };
+
+            foreach (var familia in familias)
+            {
+                Console.WriteLine(string.Join(", ", familia));
+            }
+        }
+    }
+}
